f83e817dc693a95ae79f240b9d9e767cbb9c2d56
Chaithanya Teja <munnark1999@gmail.com>
2025-11-07 19:32:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Marketplace Backend!' });
+});
+
+app.listen(PORT, () => console.log('✅ Server running on port ' + PORT));
